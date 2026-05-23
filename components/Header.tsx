e14bb49e454b8731ca9283275b6e440ccdc4e1b1
godivatech <infogodivatech@gmail.com>
2026-05-23 13:03:38 +0530

    code changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 glass border-b-0">
       <div className="container mx-auto px-4 md:px-6 h-20 flex items-center justify-between">
         <Link href="/" className="flex items-center">
-          <div className="relative w-[256px] h-[64px]">
+          <div className="relative w-[300px] h-[75px]">
             <Image 
               src="/images/Logo.png" 
               alt="True Waves Scaff" 
